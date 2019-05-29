a0b6ddff7ed47ba73cbd308848fa2b60ae6d9b0b
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-28 19:20:55 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,11 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'bootstrap';
 import '../styles/main.scss';
+import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   console.error('testing, hello?');
 };
 init();
